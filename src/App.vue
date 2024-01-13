542c17f652223648658462ebcc79defc2e3fc116
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-13 02:57:36 -0500

    ternary operator

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 const msg = 'Hello Vue 3.0';
 const styleColor = 'color: green';
+const valid = true;
 </script>
 
 <template>
@@ -10,6 +11,10 @@ const styleColor = 'color: green';
 
 	<!-- <h2 style="color: blue">Example of inline style</h2> -->
 	<h2 :style="styleColor">Example of inline style</h2>
+
+	<h2>
+		{{ valid ? 'Valid' : 'Not Valid' }}
+	</h2>
 </template>
 
 <style>
