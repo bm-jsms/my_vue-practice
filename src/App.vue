da36c8c8c7745cf8bc3fc75db43ffa98a91ebdc6
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-13 02:16:58 -0500

    v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
-  const msg = 'Hello Vue 3.0'
+const msg = 'Hello Vue 3.0';
+const styleColor = 'color: green';
 </script>
 
 <template>
 	<h1>
-    {{ msg }}
-  </h1>
+		{{ msg }}
+	</h1>
+
+	<!-- <h2 style="color: blue">Example of inline style</h2> -->
+	<h2 v-bind:style="styleColor">Example of inline style</h2>
 </template>
 
 <style>
